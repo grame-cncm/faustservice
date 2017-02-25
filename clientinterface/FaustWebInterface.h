54edbdea06084a42d19a5074817c728bbfdf1312
Stephane Letz <letz@grame.fr>
2017-02-25 12:56:16 +0100

    README reworked in markdown, cleanup.

--- clientinterface/FaustWebInterface.h
+++ clientinterface/FaustWebInterface.h
@@ -1,11 +1,34 @@
+/************************************************************************
+ FAUST Architecture File
+ Copyright (C) 2017 GRAME, Centre National de Creation Musicale
+ ---------------------------------------------------------------------
+ This Architecture section is free software; you can redistribute it
+ and/or modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 3 of
+ the License, or (at your option) any later version.
+ 
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with this program; If not, see <http://www.gnu.org/licenses/>.
+ 
+ EXCEPTION : As a special exception, you may create a larger work
+ that contains this FAUST architecture section and distribute
+ that work under terms of your choice, so long as this FAUST
+ architecture section is not modified.
+ 
+ ************************************************************************
+ ************************************************************************/
+
 #ifndef FAUSTWEBINTERFACE_H
 #define FAUSTWEBINTERFACE_H
 
 #include <string>
 #include <vector>
 #include <map>
-
-using namespace std;
 
 /* Access to FaustWeb service - Requests the supported platforms and architecture of a FaustWeb service
  * 
@@ -16,11 +39,11 @@ using namespace std;
  *
  * @return true if no error was encountered
  */
-bool fw_get_available_targets(const std::string& url, std::map<std::string, std::vector<std::string> >& targets, string& error);
+bool fw_get_available_targets(const std::string& url, std::map<std::string, std::vector<std::string> >& targets, std::string& error);
 
 //---------------------EXPORT IN 1 FUNCTION CALL
 
-/* Access to FaustWeb service - Upload your Faust application given a sourceFile, an operating system and an architecture
+/* Access to FaustWeb service - Upload your Faust application given a sourcefile, an operating system and an architecture
  *
  * @param url - FaustWeb server URL
  * @param filename - Faust source file
