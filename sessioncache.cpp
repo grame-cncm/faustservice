61aaa60eeafc2de4dfbddf6dc772166c45b1ce97
Yann Orlarey <orlarey@gmail.com>
2018-12-18 03:31:53 +0100

    Use path instead of string

--- sessioncache.cpp
+++ sessioncache.cpp
@@ -1,41 +1,23 @@
-/* We can use stl container list as a double
-ended queue to store the cache keys, with
-the descending time of reference from front
-to back and a set container to check presence
-of a key. But to fetch the address of the key
-in the list using find(), it takes O(N) time.
-    This can be optimized by storing a reference
-    (iterator) to each key in a hash map. */
-
-#include <boost/filesystem.hpp>
-#include <boost/utility.hpp>
-#include <iostream>
-#include <list>
-#include <string>
-#include <unordered_map>
-
-namespace fs = boost::filesystem;
-
 #include "sessioncache.hh"
 
-using namespace std;
-
-SessionCache::SessionCache(const fs::path& sessionDir, int size) : fSessionDir(sessionDir), fMaxSize(size)
+SessionCache::SessionCache(const fs::path& sessionDir, int size) : fSessionsDir(sessionDir), fMaxSize(size)
 {
-    initFromSessionDir();
+    for (const auto& x : fs::directory_iterator(fSessionsDir)) {
+        refer(x.path().filename());
+    }
 }
 
 /* Refers key x with in the LRU cache */
-void SessionCache::refer(string x)
+void SessionCache::refer(const fs::path& x)
 {
-    cerr << "REFER TO " << x << endl;
+    std::cerr << "REFER TO " << x << std::endl;
 
     // not present in cache
     if (fCachePos.find(x) == fCachePos.end()) {
         // cache is full
         if (fCacheList.size() == fMaxSize) {
             // delete least recently used element
-            string last = fCacheList.back();
+            fs::path last = fCacheList.back();
             fCacheList.pop_back();
             fCachePos.erase(last);
             dispose(last);
@@ -49,27 +31,13 @@ void SessionCache::refer(string x)
     fCachePos[x] = fCacheList.begin();
 }
 
-// display contents of cache
-void SessionCache::display()
-{
-    for (auto s : fCacheList) cout << s << " ";
-    cout << endl;
-}
-
 // dispose an element that leave the cache
-void SessionCache::dispose(string s)
+void SessionCache::dispose(const fs::path& s)
 {
-    cerr << "DISPOSE OF " << s << endl;
+    std::cerr << "DISPOSE OF " << s << std::endl;
     try {
-        fs::remove_all(fSessionDir / s);
+        fs::remove_all(fSessionsDir / s);
     } catch (const boost::filesystem::filesystem_error& e) {
-        cerr << "***ERROR removing " << s << " : " << e.code().message() << endl;
-    }
-}
-
-void SessionCache::initFromSessionDir()
-{
-    for (auto x : fs::directory_iterator(fSessionDir)) {
-        refer(x.path().filename().string());
+        std::cerr << "ERROR REMOVING CACHE ENTRY " << s << " : " << e.code().message() << std::endl;
     }
 }
--- sessioncache.hh
+++ sessioncache.hh
@@ -1,26 +1,20 @@
 #include <boost/filesystem.hpp>
-#include <boost/utility.hpp>
 #include <iostream>
 #include <list>
-#include <string>
-#include <unordered_map>
+#include <map>
 
 namespace fs = boost::filesystem;
 
-using namespace std;
-
 class SessionCache {
-    const fs::path fSessionDir;
-    int            fMaxSize;    // maximum capacity of cache
-    list<string>   fCacheList;  // store keys of cache
+    const fs::path      fSessionsDir;
+    int                 fMaxSize;    // maximum capacity of cache
+    std::list<fs::path> fCacheList;  // store keys of cache
 
     // store references of key in cache
-    unordered_map<string, list<string>::iterator> fCachePos;
+    std::map<fs::path, std::list<fs::path>::iterator> fCachePos;
 
    public:
     SessionCache(const fs::path& sessionDir, int size);
-    void initFromSessionDir();  // init the cache by reading the session dir
-    void refer(string);         // refer to an element in the cache
-    void display();
-    void dispose(string);  // this element is removed from the cache
+    void refer(const fs::path&);    // refer to an element in the cache
+    void dispose(const fs::path&);  // this element is removed from the cache
 };
